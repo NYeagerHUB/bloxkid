9b60eed5b4932bcc274cafd3aa0ff5bf759ccb65
NYeagerHUB <tn2431814@gmail.com>
2026-02-11 18:59:55 +0700

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,48 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "NYeagerHub",
+         Animation = "Thủ khoa khối A00"
+         },
+        Key = {
+        KeySystem = true,
+        Title = "Key System",
+        Description = "CHECKPOINT",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+      Image = "rbxassetid://1234567890"
+",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "BananaCatHub"})
+     
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "Main",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
+  end
+  })
