0c1bf054e79cbb171b0836519f45765aa37e1edf
NYeagerHUB <tn2431814@gmail.com>
2026-02-11 19:02:49 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
        local Window = MakeWindow({
          Hub = {
@@ -45,4 +45,5 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
   end
+
   })
